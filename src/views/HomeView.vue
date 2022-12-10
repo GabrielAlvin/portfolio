36658423593b5771aaf360c97f608cc35a750071
Gabriel Souza <gabrielsouzacontato.99@gmail.com>
2022-12-10 19:10:35 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home" class="bg-black">
+      <h1 class="font-weight-bold text-md-h1 anima">GABRIEL SOUZA OLIVEIRA</h1>
+      <p class="font-weight-bold text-md-h5">Web developer - System Analyst</p>
+  </div>
+  
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style scoped>
+.anima{
+    display: inline-block;
+    border-right: 2px solid #000;
+    padding-right:3px ;
+    max-width: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    animation: pisca normal infinite .6s, escrever normal 3s steps(55) both;
+}
+
+p{
+  color: #9c27b0;
+}
+
+@keyframes pisca {
+    100%{
+        border-right-color: transparent;
+    }
+}
+
+@keyframes escrever {
+    100%{
+        max-width: 100%;
+    }
+}
+</style>
